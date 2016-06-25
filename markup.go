6110f1271c24561c2428e6059cbc68d73354f714
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2016-06-24 21:58:24 -0700

    new component architecture

--- markup.go
+++ markup.go
@@ -7,95 +7,6 @@ import (
 	"github.com/gopherjs/gopherjs/js"
 )
 
-// Markup represents some markup that can be applied to a DOM element. For
-// example, styles like font size, properties like checked status of an input,
-// or child elements.
-type Markup interface {
-	// Apply should apply the markup to the given element.
-	Apply(element *Element)
-}
-
-type property struct {
-	Name  string
-	Value interface{}
-}
-
-// Apply implements the Markup interface.
-func (p *property) Apply(element *Element) {
-	if element.Properties == nil {
-		element.Properties = make(map[string]interface{})
-	}
-	if _, ok := element.Properties[p.Name]; ok {
-		panic(fmt.Sprintf("duplicate property: %s", p.Name))
-	}
-	element.Properties[p.Name] = p.Value
-}
-
-// Property returns Markup which applies the given value to the named property
-// of a DOM element.
-func Property(name string, value interface{}) Markup {
-	return &property{Name: name, Value: value}
-}
-
-type data struct {
-	name  string
-	value string
-}
-
-// Apply implements the Markup interface.
-func (d *data) Apply(element *Element) {
-	if element.Dataset == nil {
-		element.Dataset = make(map[string]string)
-	}
-	if _, ok := element.Dataset[d.name]; ok {
-		panic(fmt.Sprintf("duplicate data: %s", d.name))
-	}
-	element.Dataset[d.name] = d.value
-}
-
-// Data returns Markup which applies the given value to the named custom data
-// attribute of a DOM element.
-func Data(name, value string) Markup {
-	return &data{name: name, value: value}
-}
-
-// ClassMap is markup that specifies classes to be applied to an element if
-// their boolean value are true.
-type ClassMap map[string]bool
-
-// Apply implements the Markup interface.
-func (m ClassMap) Apply(element *Element) {
-	var classes []string
-	for name, active := range m {
-		if active {
-			classes = append(classes, name)
-		}
-	}
-	Property("className", strings.Join(classes, " ")).Apply(element)
-}
-
-type style struct {
-	Name  string
-	Value interface{}
-}
-
-// Apply implements the Markup interface.
-func (s *style) Apply(element *Element) {
-	if element.Style == nil {
-		element.Style = make(map[string]interface{})
-	}
-	if _, ok := element.Style[s.Name]; ok {
-		panic(fmt.Sprintf("duplicate style: %s", s.Name))
-	}
-	element.Style[s.Name] = s.Value
-}
-
-// Style returns Markup which applies the style with the given value to an
-// element.
-func Style(name string, value interface{}) Markup {
-	return &style{Name: name, Value: value}
-}
-
 // EventListener is markup that specifies a callback function to be invoked when
 // the named DOM event is fired.
 type EventListener struct {
@@ -113,9 +24,8 @@ func (l *EventListener) PreventDefault() *EventListener {
 	return l
 }
 
-// Apply implements the Markup interface.
-func (l *EventListener) Apply(element *Element) {
-	element.EventListeners = append(element.EventListeners, l)
+func (l *EventListener) Apply(h *HTML) {
+	h.EventListeners = append(h.EventListeners, l)
 }
 
 // Event represents a DOM event.
@@ -123,20 +33,81 @@ type Event struct {
 	Target *js.Object
 }
 
-// List represents a list of markup which will all be applied to an element.
-type List []Markup
+type MarkupOrComponentOrHTML interface{}
 
-// Apply implements the Markup interface.
-func (g List) Apply(element *Element) {
-	for _, m := range g {
-		if m != nil {
-			m.Apply(element)
-		}
+func apply(m MarkupOrComponentOrHTML, h *HTML) {
+	if m == nil {
+		return
+	}
+	switch m := m.(type) {
+	case Markup:
+		m.Apply(h)
+	case Component:
+		h.Children = append(h.Children, m)
+	case *HTML:
+		h.Children = append(h.Children, m)
+	default:
+		panic(fmt.Sprintf("vecty: invalid type %T does not match MarkupOrComponent interface", m))
 	}
 }
 
-// If returns nil if cond is false, otherwise it returns a list of markup.
-func If(cond bool, markup ...Markup) Markup {
+type Markup interface {
+	Apply(h *HTML)
+}
+
+type markupFunc func(h *HTML)
+
+func (m markupFunc) Apply(h *HTML) { m(h) }
+
+func Style(key, value string) Markup {
+	return markupFunc(func(h *HTML) {
+		if h.Styles == nil {
+			h.Styles = make(map[string]string)
+		}
+		h.Styles[key] = value
+	})
+}
+
+func Property(key string, value interface{}) Markup {
+	return markupFunc(func(h *HTML) {
+		if h.Properties == nil {
+			h.Properties = make(map[string]interface{})
+		}
+		h.Properties[key] = value
+	})
+}
+
+func Data(key, value string) Markup {
+	return markupFunc(func(h *HTML) {
+		h.Dataset[key] = value
+	})
+}
+
+// ClassMap is markup that specifies classes to be applied to an element if
+// their boolean value are true.
+type ClassMap map[string]bool
+
+// Apply implements the Markup interface.
+func (m ClassMap) Apply(h *HTML) {
+	var classes []string
+	for name, active := range m {
+		if active {
+			classes = append(classes, name)
+		}
+	}
+	Property("className", strings.Join(classes, " ")).Apply(h)
+}
+
+type List []MarkupOrComponentOrHTML
+
+func (l List) Apply(h *HTML) {
+	for _, m := range l {
+		apply(m, h)
+	}
+}
+
+// If returns nil if cond is false, otherwise it returns the given markup.
+func If(cond bool, markup ...MarkupOrComponentOrHTML) MarkupOrComponentOrHTML {
 	if cond {
 		return List(markup)
 	}
